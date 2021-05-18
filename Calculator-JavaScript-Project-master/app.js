4d2543f7e2faaa4fc2bf5c5cab9ab0eac3a6c7ae
cxz5309 <42149645+cxz5309@users.noreply.github.com>
2021-05-18 23:58:13 +0900

    10. Calculator Project

--- Calculator-JavaScript-Project-master/app.js
+++ Calculator-JavaScript-Project-master/app.js
@@ -1,30 +1,54 @@
-//Wrap code in an IIFE
-(function(){
-  
-  let screen = document.querySelector('.screen');
-  let buttons = document.querySelectorAll('.btn');
-  let clear = document.querySelector('.btn-clear');
-  let equal = document.querySelector('.btn-equal');
-  
-  //retrieve data from numbers that are clicked
-  buttons.forEach(function(button){
-    button.addEventListener('click', function(e){
-      let value = e.target.dataset.num;
-      screen.value += value;
-    })
+let screen = document.querySelector(".screen");
+let clearBtn = document.querySelector(".btn-clear");
+let equalBtn = document.querySelector(".btn-equal");
+let numBtns = document.querySelectorAll(".btn.btn-grey");
+let calBtns = document.querySelectorAll(".btn.btn-yellow");
+
+function onCalculateBtnClick(e){
+    //according to the W3C DOM Level 2 Event model
+    //let targetText = event.target.dataset.num
+    //But of course, IE is different, so the vanilla JavaScript way of handling this is
+    e = e || window.event;
+    let targetText = e.target || e.srcElement || e;
+    if (targetText.nodeType == 3) targetText = targetText.parentNode; // defeat Safari bug
+
+    let text = targetText.dataset.num;
+    if(screen.value.includes('.') && text === '.'){
+      return;
+    } 
+    screen.value += text;
+}
+
+function onClearBtnClick(){
+  screen.value = '';
+}
+
+function onEqualBtnClick(){
+  let tmpText = calculate(screen.value);
+  screen.value = tmpText;
+}
+
+function calculate(text){
+  try{
+    return Function('"use strict";return (' + text + ')')();
+  }
+  catch(e){
+    return '';
+  }
+}
+
+
+function init(){
+  numBtns.forEach(btn =>{
+    btn.addEventListener("click", onCalculateBtnClick);
+  });
+  calBtns.forEach(btn =>{
+    btn.addEventListener("click", onCalculateBtnClick);
   });
   
-  equal.addEventListener('click', function(e){
-    if(screen.value === ''){
-      screen.value = 'Please Enter a Value';
-    } else {
-      let answer = eval(screen.value);
-      screen.value = answer;
-    }
-  })
-  
-  clear.addEventListener('click', function(e){
-    screen.value = '';
-  })
- 
-})(); //end IIFE
+  clearBtn.addEventListener("click", onClearBtnClick);
+  equalBtn.addEventListener("click", onEqualBtnClick);
+}
+
+
+init();
